c9d7530dad5029efb3c2c4affd556a83466081b9
katesem <katewhite922@gmail.com>
2023-11-13 10:38:39 +0200

    add constant class for API entities

---
+++ src/main/java/com/example/core/constants/APIConstants.java
@@ -0,0 +1,17 @@
+package com.example.core.constants;
+
+public class APIConstants {
+    //endpoints
+    public static final String BASE_URL = "https://swapi.dev/api/";
+    public static final String PEOPLE_MOCK_ENDPOINT = "people/\\d+";
+    public static String PEOPLE_ID_ENDPOINT = "people/{id}";
+
+
+    //headers
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String CONTENT_TYPE = "Content-Type";
+
+
+    //path params
+    public static final String ID = "id";
+}
